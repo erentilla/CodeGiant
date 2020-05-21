74725f42f7497062f1b06e9d86b2b5d2ebd69b98
talhaburakc <talhaburakcuhadar@gmail.com>
2020-05-22 00:59:46 +0300

    asdasd

--- src/API/index.js
+++ src/API/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 var mysql = require('mysql');
 var cors = require('cors');
+var Database = require('./Database.js');
 
 
 const API_ENDPOINT = "";
@@ -23,182 +24,262 @@ app.use(cors());
 let users  = [];
 let tracks = [];
 
+// user get
+app.get('/users', (req, res) => {
+    res.send(users);
+});
 
-{//////////////////////////////////////// ROUTES
+// user post
+app.post('/users', (req, res) => {
+    const {error} = validateUser(req.body); // result.error
+    if(error) return res.status(400).send(result.error.details[0].message);
+    
+    let user = {
+        userID: req.body.userID,
+        username: req.body.username,
+        password: req.body.password,
+        role: req.body.role
+    };
+    users.push(user);
+    res.send(user);
+});
+
+    
+// TRACK ROUTES
+// track get
+app.get('/tracks', (req, res) => {
+    /**
+        let result = {
+            "tracks": [
+                {"id": 0, "name": "track0", "subject": "DFS", "difficulty": "hard", "duration": "5 mins"},
+                {"id": 3, "name": "track3", "subject": "DFS", "difficulty": "hard", "duration": "15 mins"},
+                {"id": 4, "name": "track4", "subject": "BFS", "difficulty": "easy", "duration": "52 mins"}
+            ]
+             */
+        let database = new Database(config);
+
+        let sql = `
+            SELECT track_id AS id, track.name AS name, subjects.name AS subject, difficulty, duration
+            FROM track, subjects
+            WHERE track.subject_id = subjects.subject_id
+            ORDER BY creation_date DESC;
+        `;
+
+        database.query(sql).then(result => {
+            console.log("tracks result: ", result);
+            let send = {
+                tracks: result
+            };
+            res.json(send);
+        }).then(() => {
+            database.close();
+        });
+});
+
+// EXTRA ROUTES
+    // log-in get
+app.get('/log-in', (req, res) => {
+    
+    let username = req.query.username;
+    let password = req.query.password;
+    /**
+    console.log('log in - username: ', username);
+    console.log('log in - password: ', password);
+     */
+    let database = new Database(config);
+
+    let sql = `
+        SELECT *
+        FROM users
+        WHERE name = '${username}' AND password = '${password}';
+    `; 
+
+    database.query(sql).then((result) => {
+        console.log(result);
+        if (result === null || result.length < 1) {
+            res.status(400).send('The user with the given parameters is not found.');
+            return;
+        }
+        let queryResult = result[0];
+        let send = {
+            role: queryResult.type,
+            userid: queryResult.user_id
+        };
+        res.json(send);
+    }).then(() => {
+        database.close();
+    });
+});
+
+app.get('/questions', (req, res) => {
     
     /**
-    app.get('/', (req, res) => {
-        res.send('Home');
+    console.log('log in - username: ', username);
+    console.log('log in - password: ', password);
+     */
+    /**
+    var connection = mysql.createConnection(config); // TODO: sil?
+    connection.connect(function(err) {
+        if (err) throw err;
+        console.log("CONNECTED TO DB!");
     });
-    */
-    
-    // MAIN ROUTES
-    
-    {// USER ROUTES
-        // user get
-        app.get('/users', (req, res) => {
-            res.send(users);
-        });
+ */
+    let sql = `
+        SELECT question.question_id AS id, title, question.question as description
+	    FROM question, subjects, 
+		(SELECT question_id, MIN(subject_id) AS subject_id
+			FROM has_subject
+            GROUP BY question_id) S
+	    WHERE S.question_id = question.question_id
+		AND subjects.subject_id = S.subject_id;
+    `;
+    let database = new Database(config);
+    database.query(sql).then((result) => {
+        //if (err) throw err;
+        console.log(result);
+        if (result === null || result.length < 1) {
+            res.status(400).send('No questions??? problem error???.');
+            return;
+        }
+        res.json({questions: result});
+    }).then(() => {
+        database.close();
+    });
         
-        // user post
-        app.post('/users', (req, res) => {
-            const {error} = validateUser(req.body); // result.error
-            if(error) return res.status(400).send(result.error.details[0].message);
-            
-            let user = {
-                userID: req.body.userID,
-                username: req.body.username,
-                password: req.body.password,
-                role: req.body.role
-            };
-            users.push(user);
-            res.send(user);
-        });
-    }
-    
-    {// TRACK ROUTES
-        // track get
-        app.get('/tracks', (req, res) => {
-            let result = {
-                "tracks": [
-                    {"id": 0, "name": "track0", "subject": "DFS", "difficulty": "hard", "duration": "5 mins"},
-                    {"id": 3, "name": "track3", "subject": "DFS", "difficulty": "hard", "duration": "15 mins"},
-                    {"id": 4, "name": "track4", "subject": "BFS", "difficulty": "easy", "duration": "52 mins"}
-                ]
-            };
-            res.send(result);
-        });
-        
-        // track post //TO BE DEVELOPED
-        app.post('/tracks', (req, res) => {
-            const {error} = validateTrack(req.body); // result.error
-            if(error) return res.status(400).send(result.error.details[0].message);
-            
-            let track = {
-                trackID     : req.body.trackID,
-                trackName   : req.body.trackName,
-                subject     : req.body.subject,
-                difficulty  : req.body.difficulty,
-                duration    : req.body.duration 
-            };
-            tracks.push(track);
-            res.send(track);
-        });
-    }
-    
-    {// EXTRA ROUTES
-        // log-in get
-        app.get('/log-in', (req, res) => {
-            let notFound = false;
-            if (notFound) res.status(400).send('The user with the given parameters is not found.');
-            let userid = req.query.userid;
-            let password = req.query.password;
-            console.log('log in - userid: ', userid);
-            console.log('log in - password: ', password);
+    //connection.end();
+});
 
-            var connection = mysql.createConnection(config);
-            connection.connect(function(err) {
-                if (err) throw err;
-                console.log("CONNECTED TO DB!");
-            });
+// completed-tracks get //DATABASE CONNECTION
+app.get('/completed-tracks', (req, res) => {
+    let notFound = false;
+    if (notFound) res.status(400).send('The tracks with the given parameters are not found.');
+    let userid = req.query.userid;
+    console.log('completed tracks - userid: ', userid);
 
-            let sql = `
-                SELECT *
-	            FROM users
-                WHERE name = '${userid}' AND password = '${password}';
+    let sql = `
+        SELECT report.track_id AS id, track.name AS name, subjects.name AS subject, difficulty AS difficulty, finish_time-start_time AS duration
+        FROM report, subjects, track
+        WHERE user_id = ${userid}
+        AND report.track_id = track.track_id
+        AND track.subject_id = subjects.subject_id
+    `;
+
+
+    let database = new Database(config);
+    database.query(sql).then(rows => {
+        res.json({tracks: rows});
+    }).then(() => database.close());
+});
+
+// questions-of-track //DATABASE CONNECTION
+app.get('/questions-of-track', (req, res) => {
+    let notFound = false;
+    if (notFound) res.status(400).send('The track with the given parameters is not found.');
+    let trackid = req.query.trackid;
+    console.log('questions of track - trackid: ', trackid);
+    let result = {
+        "questions": [
+            {"id": 4, "title": "question4", "description": "blablablablabla", "test-cases": [{"case-id": 0, "case": 1}]},
+            {"id": 0, "title": "question0", "description": "LMAOOO", "test-cases": [{"case-id": 0, "case": 1}]},
+            {"id": 3, "title": "question3", "description": "XDXDXDXD", "test-cases": [{"case-id": 0, "case": 1}]}
+        ]
+    }
+    res.json(result);
+});
+
+// report get //DATABASE CONNECTION
+app.get('/report', (req, res) => {
+    let notFound = false;
+    if (notFound) res.status(400).send('The report with the given parameters are not found.');
+    let userid = req.query.userid;
+    let trackid = req.query.trackid;
+    console.log('report - userid: ', userid);
+    console.log('report - trackid: ', trackid);
+    let result = {
+        "question_results": [
+            {"question_id": 0, "question_title": "a nice question", "score": 100},
+            {"question_id": 1, "question_title": "a terrible question", "score": 5}
+        ],
+        "total_score": 90
+    };
+    res.json(result);
+});
+
+app.post('/create-question', (req, res) => {
+    let userid = req.body.userid;
+    let question = req.body.question;
+    let testcases = req.body.testcases;
+    let language = req.body.language;
+    let subject = req.body.subject;
+    let difficulty = req.body.difficulty;
+    let title = 'titlexd' // TODO: add title :D
+    console.log(req.body);
+
+    let database = new Database(config);
+	let insertQuestion = `
+                INSERT INTO question (writer_id, title, question)
+			    VALUES ( ${userid}, '${title}', '${question}')
+            `;
+    
+
+ 	database.query(insertQuestion);
+
+	let lastInsertId = `
+                SELECT LAST_INSERT_ID() AS id
+            `;
+	let question_id = 1;
+    database.query(lastInsertId).then(result => {
+        question_id = result[0].id
+        console.log("- ----------QUESTION ID: ", question_id);
+    }).then(() => {
+        let insertSubject = `
+                INSERT INTO has_subject (question_id, subject_id)
+			    VALUES (${question_id},
+				(SELECT subject_id
+				FROM subjects
+                		WHERE name = '${subject}')
+				)
             `;
 
-            connection.query(sql, (err, result, fields) => {
-                if (err) throw err;
-                console.log(result);
-                let queryResult = result[0];
-                let send = {
-                    role: queryResult.type,
-                    userid: queryResult.user_id
-                };
-                res.json(send);
-            });
-             
-            connection.end();
-        });
+        database.query(insertSubject);
         
-        // completed-tracks get //DATABASE CONNECTION
-        app.get('/completed-tracks', (req, res) => {
-            let notFound = false;
-            if (notFound) res.status(400).send('The tracks with the given parameters are not found.');
-            let userid = req.query.userid;
-            console.log('completed tracks - userid: ', userid);
-            let result = {
-                "tracks": [
-                    {"id": 0, "name": "track0 completed", "subject": "DFS", "difficulty": "hard", "duration": "5 mins"},
-                    {"id": 3, "name": "track3  completed", "subject": "DFS", "difficulty": "hard", "duration": "15 mins"},
-                    {"id": 4, "name": "track4  completed", "subject": "BFS", "difficulty": "easy", "duration": "52 mins"}
-                ]
-            }
-            res.json(result);
-        });
-        
-        // questions-of-track //DATABASE CONNECTION
-        app.get('/questions-of-track', (req, res) => {
-            let notFound = false;
-            if (notFound) res.status(400).send('The track with the given parameters is not found.');
-            let trackid = req.query.trackid;
-            console.log('questions of track - trackid: ', trackid);
-            let result = {
-                "questions": [
-                    {"id": 4, "title": "question4", "description": "blablablablabla", "test-cases": [{"case-id": 0, "case": 1}]},
-                    {"id": 0, "title": "question0", "description": "LMAOOO", "test-cases": [{"case-id": 0, "case": 1}]},
-                    {"id": 3, "title": "question3", "description": "XDXDXDXD", "test-cases": [{"case-id": 0, "case": 1}]}
-                ]
-            }
-            res.json(result);
-        });
-        
-        // report get //DATABASE CONNECTION
-        app.get('/report', (req, res) => {
-            let notFound = false;
-            if (notFound) res.status(400).send('The report with the given parameters are not found.');
-            let userid = req.query.userid;
-            let trackid = req.query.trackid;
-            console.log('report - userid: ', userid);
-            console.log('report - trackid: ', trackid);
-            let result = {
-                "question_results": [
-                    {"question_id": 0, "question_title": "a nice question", "score": 100},
-                    {"question_id": 1, "question_title": "a terrible question", "score": 5}
-                ],
-                "total_score": 90
-            };
-            res.json(result);
-        });
-        
-        // submit-track get PROBLEMATIC
-        app.post('/submit-track', (req, res) => {
-            let userid = req.body.userid;
-            let submission = req.body.submission;
-            console.log('submit track - userid: ', userid);
-            console.log("submit track - submission: ", submission);
-            res.send();
-        });
-        
+        for (let i = 0; i < testcases.length; i++) {
+            let sql = `
+                        INSERT INTO test_case (question_id, input, output)
+                        VALUES (${question_id}, '${testcases[i][0]}', '${testcases[i][1]}')
+                    `;
 
-        app.get('/leaderboard', (req, res) => {
-            let notFound = false;
-            if (notFound) res.status(400).send('The leaderboard with the given parameters are not found.');
-            let trackid = req.query.trackid;
-            console.log("leaderboard - trackid: ", trackid);
-            let result = {
-                "leaderboard": [
-                    {"user_id": "baba", "score": 100},
-                    {"user_id": "mehmet", "score": 70},
-                    {"user_id": "lmao", "score": 50},
-                    {"user_id": "alko", "score": 0}
-                ]
-            };
-            res.json(result);
-        });
-    }
-}
+            database.query(sql);
+        }
+        res.json();
+    });
+});
+
+app.post('/submit-track', (req, res) => {
+    let userid = req.body.userid;
+    let submission = req.body.submission;
+    console.log('submit track - userid: ', userid);
+    console.log("submit track - submission: ", submission);
+    res.send();
+});
+
+
+app.get('/leaderboard', (req, res) => {
+    let notFound = false;
+    if (notFound) res.status(400).send('The leaderboard with the given parameters are not found.');
+    let trackid = req.query.trackid;
+    console.log("leaderboard - trackid: ", trackid);
+    let result = {
+        "leaderboard": [
+            {"user_id": "baba", "score": 100},
+            {"user_id": "mehmet", "score": 70},
+            {"user_id": "lmao", "score": 50},
+            {"user_id": "alko", "score": 0}
+        ]
+    };
+    res.json(result);
+});
+
 
 
 
@@ -232,45 +313,6 @@ function validateTrack(track){
 // PORT
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
-
-
-let createUserSql = `
-INSERT INTO users (name, email, password, birth_date)
-	VALUES (
-            'talha',
-            'talha@t.com',
-            '123',
-            '2010-01-10'
-	)
-`;
-
-let sql = `
-INSERT INTO user_type (user_id, type)
-    VALUES (
-            (SELECT AUTO_INCREMENT - 1
-                FROM information_schema.TABLES
-                WHERE TABLE_SCHEMA = "codegiant"
-                AND TABLE_NAME = "users"),
-            "dev"
-    )
-`;
-
-/*
-connection.query(sql, (err, result, fields) => {
-    if (err) throw err;
-    console.log(result);
-});
- */
-
-
-/**
-connection.query("SELECT * FROM USER_TYPE", (err, result, fields) => {
-    if (err) throw err;
-    console.log(result);
-});
- 
-connection.end();
- */
 
 
 
--- src/SQL/query1.sql
+++ src/SQL/query1.sql
@@ -2,10 +2,66 @@ INSERT INTO users (name, email, password, birth_date, type)
 	VALUES (
 			'tilla',
             'tilla@gmail.com',
-            '12312312',
+            '123',
             '2010-10-10',
-            'adm'
+            'dev'
 	);
 
-DELETE FROM USERS WHERE type = 'adm';
-SELECT * FROM USERS
+SELECT *
+	FROM users
+    WHERE name = 'talha' AND password = '123';
+
+
+SELECT track_id AS id, track.name AS name, subjects.name AS subject, difficulty, duration
+FROM track, subjects
+WHERE track.subject_id = subjects.subject_id
+ORDER BY creation_date DESC;
+
+SELECT question_id, title, question, writer_id, difficulty
+	FROM question
+    WHERE question_id IN (SELECT question_id
+							FROM consists_of
+                            WHERE track_id = 3);
+
+SELECT question.question_id AS id, title, question.question as description
+	FROM question, subjects, 
+		(SELECT question_id, MIN(subject_id) AS subject_id
+			FROM has_subject
+            GROUP BY question_id) S
+	WHERE S.question_id = question.question_id
+		AND subjects.subject_id = S.subject_id;
+
+INSERT INTO track (writer_id, subject_id, name, difficulty, duration)
+	VALUES (6,
+			(SELECT subject_id
+				FROM subjects
+                WHERE subjects.name = 'DFS'),
+			'track0',
+            'easy',
+            '6:35:01');
+
+INSERT INTO subjects (name, description)
+	VALUES ('DFS', 'DFS iste');
+
+SELECT * FROM USERS;
+
+INSERT INTO question (writer_id, title, question)
+	VALUES ( 7, 'demo question', '??????**??');
+    
+INSERT INTO has_subject (question_id, subject_id)
+	VALUES (1,
+			(SELECT subject_id
+				FROM subjects
+                WHERE name = 'DFS')
+	);
+    
+INSERT INTO test_case (question_id, input, output)
+	VALUES (1, 'demo input', 'demo output');
+    
+INSERT INTO consists_of (track_id, question_id)
+	VALUES (3, 1);
+    
+SELECT * FROM question;
+SELECT * FROM test_case;
+
+DELETE FROM USERS WHERE type IS NOT NULL;
