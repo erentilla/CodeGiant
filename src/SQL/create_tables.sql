f7754794d801e814cd6bd4e593dd5f9109166a58
Alkım Önen <alkimonen@hotmail.com>
2020-05-21 18:09:50 +0300

    SQL queries

---
+++ src/SQL/create_tables.sql
@@ -0,0 +1,184 @@
+-- Drop tables if exists
+DROP TABLE IF EXISTS replied_to;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS interview_request;
+DROP TABLE IF EXISTS follow;
+DROP TABLE IF EXISTS represents;
+DROP TABLE IF EXISTS company;
+DROP TABLE IF EXISTS participation;
+DROP TABLE IF EXISTS report;
+DROP TABLE IF EXISTS submitted_code;
+DROP TABLE IF EXISTS is_featured;
+DROP TABLE IF EXISTS consists_of;
+DROP TABLE IF EXISTS track;
+DROP TABLE IF EXISTS has_subject;
+DROP TABLE IF EXISTS test_case;
+DROP TABLE IF EXISTS question;
+DROP TABLE IF EXISTS subjects;
+DROP TABLE IF EXISTS user_type;
+DROP TABLE IF EXISTS users;
+
+-- Create tables
+-- User related tables
+CREATE TABLE users(
+user_id	int NOT NULL AUTO_INCREMENT,
+name	VARCHAR(20) NOT NULL,
+email	VARCHAR(30) NOT NULL,
+password	VARCHAR(20) NOT NULL,
+birth_date	DATETIME,
+user_bio	VARCHAR(100),
+PRIMARY KEY (user_id)
+);
+CREATE TABLE user_type(
+user_id		int not null,
+type		ENUM("dev", "adm", "rep") NOT NULL,
+PRIMARY KEY (user_id),
+CONSTRAINT fk_type_user FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+-- Question related tables
+CREATE TABLE subjects(
+subject_id	int NOT NULL AUTO_INCREMENT,
+name		VARCHAR(10) NOT NULL,
+description	VARCHAR(100) NOT NULL,
+PRIMARY KEY (subject_id)
+);
+CREATE TABLE question(
+question_id		INT NOT NULL AUTO_INCREMENT,
+writer_id		INT NOT NULL,
+assessor_id		INT,
+title			VARCHAR(15) NOT NULL,
+question		VARCHAR(100) NOT NULL,
+difficulty		ENUM ("easy", "medium", "hard", "extreme"),
+status			ENUM( "waiting", "approved", "declined") DEFAULT ("waiting"),
+creation_date	DATETIME DEFAULT CURRENT_TIMESTAMP,
+assessment_date	DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (question_id),
+CONSTRAINT fk_question_writer FOREIGN KEY (writer_id) REFERENCES users(user_id),
+CONSTRAINT fk_question_assessor FOREIGN KEY (assessor_id) REFERENCES users(user_id)
+);
+CREATE TABLE test_case(
+case_id		INT NOT NULL AUTO_INCREMENT,
+question_id	INT NOT NULL,
+input		VARCHAR(50),
+output		VARCHAR(50) NOT NULL,
+PRIMARY KEY (case_id, question_id),
+CONSTRAINT fk_test_question	FOREIGN KEY (question_id) REFERENCES question(question_id)
+);
+CREATE TABLE has_subject(
+question_id		INT NOT NULL,
+subject_id		INT NOT NULL,
+PRIMARY KEY (question_id, subject_id),
+CONSTRAINT fk_has_subject_question FOREIGN KEY (question_id) REFERENCES question(question_id),
+CONSTRAINT fk_has_subject_subject FOREIGN KEY (subject_id) REFERENCES subjects(subject_id)
+);
+
+-- Track related tables
+CREATE TABLE track(
+track_id		INT NOT NULL AUTO_INCREMENT,
+writer_id		INT NOT NULL,
+subject_id		INT NOT NULL,
+name			VARCHAR(20),
+difficulty		ENUM ("easy", "medium", "hard", "extreme"),
+duration		TIME,
+creation_date	DATETIME DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (track_id),
+CONSTRAINT fk_track_user FOREIGN KEY (writer_id) REFERENCES users(user_id),
+CONSTRAINT fk_track_subject FOREIGN KEY (subject_id) REFERENCES subjects(subject_id)
+);
+CREATE TABLE consists_of(
+track_id		INT NOT NULL,
+question_id		INT NOT NULL,
+PRIMARY KEY (track_id, question_id),
+CONSTRAINT fk_consist_track FOREIGN KEY (track_id) REFERENCES track(track_id),
+CONSTRAINT fk_consist_question FOREIGN KEY (question_id) REFERENCES question(question_id)
+);
+CREATE TABLE is_featured(
+track_id		INT NOT NULL,
+feature_date	DATETIME NOT NULL,
+PRIMARY KEY (track_id),
+CONSTRAINT fk_featured_track FOREIGN KEY (track_id) REFERENCES track(track_id)
+);
+
+-- Submission related tables
+CREATE TABLE submitted_code(
+submission_id	INT NOT NULL AUTO_INCREMENT,
+language			VARCHAR(15),
+code			VARCHAR(300),
+PRIMARY KEY (submission_id)
+);
+CREATE TABLE report(
+report_id		INT NOT NULL AUTO_INCREMENT,
+user_id			INT NOT NULL,
+track_id		INT NOT NULL,
+score			INT NOT NULL,
+start_time		DATETIME NOT NULL,
+finish_time		DATETIME NOT NULL,
+PRIMARY KEY (report_id, user_id, track_id),
+CONSTRAINT fk_report_user FOREIGN KEY (user_id) REFERENCES users(user_id),
+CONSTRAINT fk_report_track FOREIGN KEY (track_id) REFERENCES track(track_id)
+);
+CREATE TABLE participation(
+track_id			INT NOT NULL,
+developer_id		INT NOT NULL,
+question_id			INT NOT NULL,
+submission_id		INT NOT NULL,
+start_time			DATETIME NOT NULL,
+finish_time			DATETIME NOT NULL,
+PRIMARY KEY (track_id, developer_id),
+CONSTRAINT fk_part_track FOREIGN KEY (track_id) REFERENCES track(track_id),
+CONSTRAINT fk_part_user FOREIGN KEY (developer_id) REFERENCES users(user_id),
+CONSTRAINT fk_part_question FOREIGN KEY (question_id) REFERENCES question(question_id),
+CONSTRAINT fk_part_code	FOREIGN KEY (submission_id) REFERENCES submitted_code(submission_id)
+);
+
+-- Company related tables
+CREATE TABLE company(
+company_id		INT NOT NULL AUTO_INCREMENT,
+name			VARCHAR(20) NOT NULL,
+bio				VARCHAR(100),
+PRIMARY KEY (company_id)
+);
+CREATE TABLE represents(
+company_id			INT NOT NULL,
+representative_id	INT NOT NULL,
+PRIMARY KEY (company_id, representative_id),
+CONSTRAINT fk_repr_company FOREIGN KEY (company_id) REFERENCES company(company_id),
+CONSTRAINT fk_repr_user FOREIGN KEY (representative_id) REFERENCES users(user_id)
+);
+CREATE TABLE follow(
+representative_id		INT NOT NULL,
+track_id				INT NOT NULL,
+PRIMARY KEY (representative_id, track_id),
+CONSTRAINT fk_follow_user FOREIGN KEY (representative_id) REFERENCES users(user_id),
+CONSTRAINT fk_follow_track FOREIGN KEY (track_id) REFERENCES track(track_id)
+);
+CREATE TABLE interview_request(
+request_id		INT NOT NULL AUTO_INCREMENT,
+sender_id		INT NOT NULL,
+receiver_id		INT NOT NULL,
+status			ENUM( "waiting", "accepted", "declined") DEFAULT ("waiting"),
+job_desc		VARCHAR(100) NOT NULL,
+sent			DATETIME NOT NULL,
+deadline		DATETIME NOT NULL,
+PRIMARY KEY (request_id),
+CONSTRAINT fk_request_sender FOREIGN KEY (sender_id) REFERENCES users(user_id),
+CONSTRAINT fk_request_receiver FOREIGN KEY (receiver_id) REFERENCES users(user_id)
+);
+
+-- Comment related tables
+CREATE TABLE comments(
+comment_id		INT NOT NULL AUTO_INCREMENT,
+question_id		INT NOT NULL,
+user_id			INT NOT NULL,
+time			DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (comment_id),
+CONSTRAINT fk_comment_question FOREIGN KEY (question_id) REFERENCES question(question_id),
+CONSTRAINT fk_comment_user FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+CREATE TABLE replied_to(
+comment_id			INT NOT NULL,
+replied_cmt_id		INT NOT NULL,
+PRIMARY KEY (comment_id),
+CONSTRAINT fk_reply_comment FOREIGN KEY (comment_id) REFERENCES comments(comment_id)
+);
---
+++ src/SQL/queries.sql
@@ -0,0 +1,261 @@
+-- Quesries
+
+-- Sign up
+INSERT INTO users (name, email, password, birth_date)
+	VALUES (
+			@name,
+            @email,
+            @password,
+            @birth_date
+	);
+INSERT INTO user_type (user_id, type)
+	VALUES (
+			(SELECT AUTO_INCREMENT - 1
+				FROM information_schema.TABLES
+				WHERE TABLE_SCHEMA = "codegiant"
+				AND TABLE_NAME = "users"),
+			"dev"
+	);
+
+-- Change password
+UPDATE USERS
+	SET password = @password
+    WHERE user_id = @user_id;
+
+-- Change bio
+UPDATE users
+	SET user_bio = @bio
+    WHERE user_id = @user_id;
+
+-- Login
+SELECT *
+	FROM users RIGHT JOIN user_type
+    ON users.user_id = user_type.user_id
+    WHERE type = @type AND name = @name AND password = @password;
+
+-- DEVELOPER QUERIES
+-- Display tracks
+SELECT track_id, users.name, subjects.name, duration, difficulty, creation_date
+	FROM track, users, subjects
+    WHERE track.subject_id = subjects.subject_id AND writer_id = user_id
+    ORDER BY creation_date DESC;
+-- Display featured tracks
+SELECT track.track_id, users.name, subjects.name, duration, difficulty, creation_date
+	FROM track, users, subjects, is_featured
+    WHERE track.track_id IN (SELECT track_id FROM is_featured)
+		AND track.subject_id = subjects.subject_id
+        AND writer_id = user_id
+    ORDER BY creation_date DESC;
+
+-- Search subjects
+SELECT track_id, users.name, subjects.name, duration, difficulty, creation_date
+	FROM track, users, subjects
+    WHERE track.subject_id = subjects.subject_id 
+		AND subjects.name = @subj
+        AND writer_id = user_id
+    ORDER BY creation_date DESC;
+
+-- Display/update interview requests
+SELECT request_id, company.name, status, sent, deadline
+	FROM interview_request, represents, company
+    WHERE receiver_id = @user_id
+		AND deadline > CURRENT_TIMESTAMP
+		AND sender_id = representative_id
+		AND represents.company_id = company.company_id;
+SELECT request_id, sender_id, company.name, company.bio, status, job_desc, sent, deadline
+	FROM interview_request, represents, company
+    WHERE request_id = @req_id
+		AND sender_id = representative_id
+		AND represents.company_id = company.company_id;
+UPDATE interview_request
+	SET status = "accepted"
+	WHERE request_id = @req_id;
+UPDATE interview_request
+	SET status = "declined"
+	WHERE request_id = @req_id;
+
+-- Track: display questions, submit answers
+-- Get questions in selected track
+SELECT question_id
+	FROM consists_of
+	WHERE track_id = @track_id;
+-- Display question
+SELECT title, question, input AS test_input, output AS test_output
+	FROM question, test_case
+    WHERE question.question_id = @q_id
+		AND test_case.question_id = @q_id
+	ORDER BY case_id;
+-- Display test_cases
+SELECT case_id, input AS test_input, output AS test_output
+	FROM test_case
+    WHERE question_id = @q_id
+	ORDER BY case_id;
+-- Submit code
+INSERT INTO submitted_code (language, code)
+	VALUES ( @lang, @code);
+-- Get id of submission
+SELECT (AUTO_INCREMENT - 1) AS submission_id
+				FROM information_schema.TABLES
+				WHERE TABLE_SCHEMA = "codegiant"
+				AND TABLE_NAME = "submitted_code";
+-- Create participation
+INSERT INTO participation (track_id, developer_id, question_id, submission_id, start_time, end_time)
+	VALUES (@track_id, @user_id, @question_id, @submission_id, @start_time, @end_time);
+
+-- Display report
+SELECT score, start_time, finish_time
+	FROM report
+    WHERE user_id = @user_id AND track_id = @track_id;
+
+-- Display submitted code for track
+SELECT question_id, participation.submission_id, language, code, start_time, finish_time
+	FROM participation, submitted_code
+    WHERE track_id = @track_id
+		AND developer_id = @user_id
+        AND participation.submission_id = submitted_code.submission_id;
+
+-- Display leaderboard
+SELECT report.user_id, users.name, score
+	FROM report LEFT JOIN users
+		ON report.user_id = users.user_id
+	WHERE track_id = @track_id
+    ORDER BY score DESC;
+
+-- Display completed tracks
+SELECT report.track_id, track.name, subjects.name, difficulty, finish_time AS completion_time, score
+	FROM report, subjects, track
+    WHERE user_id = @user_id
+		AND	report.track_id = track.track_id
+		AND track.subject_id = subjects.subject_id;
+
+-- Create question
+INSERT INTO question (writer_id, title, question)
+	VALUES ( @user_id, @title, @question);
+-- Get question_id to @q_id
+SELECT (AUTO_INCREMENT - 1) AS question_id
+				FROM information_schema.TABLES
+				WHERE TABLE_SCHEMA = "codegiant"
+				AND TABLE_NAME = "question";
+-- Assign subject
+INSERT INTO has_subject (question_id, subject_id)
+	VALUES (@q_id,
+			(SELECT subject_id
+				FROM subjects
+                WHERE name = @subj)
+	);
+-- Create test cases
+INSERT INTO test_case (question_id, input, output)
+	VALUES (@q_id, @input, @output);
+
+
+-- ADMIN QUERIES
+
+-- Display questions
+SELECT question_id, title, subjects.name, difficulty, status
+	FROM question, subjects, 
+		(SELECT question_id, MIN(subject_id) AS subject_id
+			FROM has_subject
+            GROUP BY question_id) S
+	WHERE S.question_id = question.question_id
+		AND subjects.subject_id = S.subject_id;
+
+-- Display waiting questions
+SELECT question_id, title, subjects.name, difficulty, status
+	FROM question, subjects, 
+		(SELECT question_id, MIN(subject_id) AS subject_id
+			FROM has_subject
+            GROUP BY question_id) S
+	WHERE status = "waiting"
+		AND S.question_id = question.question_id
+		AND subjects.subject_id = S.subject_id;
+
+-- Display questions by subject
+SELECT question_id, title, @subject_name, difficulty, status
+	FROM question, subjects
+	WHERE question_id IN (SELECT question_id
+							FROM has_subject, subjects
+                            WHERE subjects.name = @subject_name
+								AND has_subject.subject_id = subjects.subject_id)
+	ORDER BY creation_date;
+
+-- Create new subject
+INSERT INTO subjects (name, description)
+	VALUES (@subj_name, @subj_desc);
+
+-- Create track
+INSERT INTO track (writer_id, subject_id, name, difficulty, duration)
+	VALUES (1,
+			(SELECT subject_id
+				FROM subjects
+                WHERE subjects.name = "Java"),
+			@track_name,
+            @difficulty,
+            @duration);
+SELECT (AUTO_INCREMENT - 1) AS track_id
+				FROM information_schema.TABLES
+				WHERE TABLE_SCHEMA = "codegiant"
+				AND TABLE_NAME = "track";
+
+-- Assess question
+UPDATE question
+	SET assessor_id = @user_id,
+		status = @stat
+	WHERE question_id = @q_id;
+    
+-- Edit Question
+UPDATE question
+	SET title = @new_title, question = @new_question, difficulty = @new_difficulty
+    WHERE question_id = @q_id;
+    
+-- Add/update/delete test case
+INSERT INTO test_case (question_id, input, output)
+	VALUES (@q_id, @input, @output);
+UPDATE test_case
+	SET input = @new_input,
+		output = @new_output
+	WHERE question_id = @q_id;
+DELETE FROM test_case
+	WHERE case_id = @case_id;
+
+-- Display/edit track, add/delete questions from track
+SELECT question_id, title, question, writer_id, difficulty
+	FROM question
+    WHERE question_id IN (SELECT question_id
+							FROM consists_of
+                            WHERE track_id = @track_id);
+UPDATE track
+	SET name = @new_name, difficulty = @new_difficulty, duration = @new_duration
+    WHERE track_id = @track_id;
+INSERT INTO consists_of (track_id, question_id)
+	VALUES (@track_id, @q_id);
+DELETE FROM consists_of
+	WHERE track_id = @track_id
+		AND question_id = @q_id;
+
+-- REPRESENTATIVE QUERIES
+
+-- Display Followed Tracks
+SELECT track.track_id, users.name, subjects.name, duration, difficulty, creation_date
+	FROM track, users, subjects, follow
+    WHERE track.track_id IN (SELECT track_id
+								FROM follow
+                                WHERE representative_id = @user_id)
+		AND track.subject_id = subjects.subject_id
+        AND writer_id = user_id
+    ORDER BY creation_date DESC;
+
+-- Follow/unfollow track
+INSERT INTO follow (representative_id, track_id)
+	VALUES (@user_id, @track_id);
+DELETE FROM follow
+	WHERE representative_id = @user_id
+		AND track_id = @track_id;
+
+-- Create interview request
+INSERT INTO interview_request (sender_id, receiver_id, job_desc, deadline)
+	VALUES (@user_id, @developer_id, @desc, @deadline);
+
+-- Display interview requests
+SELECT request_id, receiver_id, status, sent, deadline
+	FROM interview_request
+    WHERE sender_id = @user_id;
